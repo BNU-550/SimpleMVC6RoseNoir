55a52d298a619768adb81af315d1215929c37af4
Derek Peacock <derek.peacock@yahoo.com>
2023-03-10 16:00:49 +0000

    Initialised database, then changed names

---
+++ SimpleMVC6RoseNoir/Data/DbInitialiser.cs
@@ -0,0 +1,96 @@
+﻿using SimpleMVC6RoseNoir.Models;
+
+namespace SimpleMVC6RoseNoir.Data
+{
+    public static class DbIntialiser
+    {
+        public static void SeedData(ApplicationDbContext context)
+        {
+            context.Database.EnsureCreated();
+
+            AddProducts(context);
+            AddVariations(context);
+        }
+
+        private static void AddProducts(ApplicationDbContext context)
+        {
+            if (context.Products.Any())
+            {
+                return;
+            }
+
+            var products = new Product[]
+            {
+                new Product()
+                {
+                     Name = "Product 1",
+                     Description = "Product 1 descriptiom ",
+                     Price = 21.00m,
+                },
+                 new Product()
+                {
+                     Name = "Product 2",
+                     Description = "Product 2 descriptiom ",
+                     Price = 22.00m,
+                },
+            };
+
+            foreach (var product in products)
+            {
+                context.Products.Add(product);
+            }
+
+            context.SaveChanges();
+        }
+        private static void AddVariations(ApplicationDbContext context)
+        {
+            if (context.Variations.Any())
+            {
+                return;
+            }
+
+            var variations = new Variation[]
+            {
+                new Variation()
+                {
+                    ProductId = 1,
+                    Name = "Yellow Product 1",
+                    Description = "Yellow Product 1 Description",
+                    Colour ="Yellow",
+                    Price = 0
+                },
+                new Variation()
+                {
+                    ProductId = 1,
+                    Name = "Blue Product 1",
+                    Description = "Blue Product 1 Description",
+                    Colour ="Blue",
+                    Price = 0
+                },
+               new Variation()
+                {
+                    ProductId = 2,
+                    Name = "Yellow Product 2",
+                    Description = "Yellow Product 2 Description",
+                    Colour ="Yellow",
+                    Price = 0
+                },
+                new Variation()
+                {
+                    ProductId = 2,
+                    Name = "Blue Product 2",
+                    Description = "Blue Product 2 Description",
+                    Colour ="Blue",
+                    Price = 0
+                }
+            };
+
+            foreach (var variation in variations)
+            {
+                context.Variations.Add(variation);
+            }
+
+            context.SaveChanges();
+        }
+    }
+}
---
+++ SimpleMVC6RoseNoir/Data/Migrations/20230310144358_InitialCreate.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SimpleMVC6RoseNoir.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ProductId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Variations",
+                columns: table => new
+                {
+                    VariationId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductId = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(120)", maxLength: 120, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(400)", maxLength: 400, nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Colour = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Variations", x => x.VariationId);
+                    table.ForeignKey(
+                        name: "FK_Variations_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "ProductId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Variations_ProductId",
+                table: "Variations",
+                column: "ProductId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Variations");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
